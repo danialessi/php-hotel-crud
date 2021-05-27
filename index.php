e26c778f2f44e139674d1f6a662525c262016361
danialessi <daniele.alessi@hotmail.it>
2021-05-27 16:36:02 +0200

    stampata lista delle stanze

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+    require_once __DIR__ . '/database.php';
+
+    $sql = "SELECT * FROM `stanze`;";
+    $result = $conn->query($sql);
+
+    $rooms = [];
+    if ($result && $result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            $rooms[] = $row;
+        }
+    }
+    // var_dump($rooms);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stanze</title>
+</head>
+<body>
+    
+    <h1>Lista Stanze</h1>
+
+    <ul>
+        <?php foreach($rooms as $room) {?>
+        <li>
+            Numero stanza: <?php echo $room['room_number'];?><br>
+            Piano: <?php echo $room['floor']?>
+        </li>
+        <?php } ?>
+    </ul>
+</body>
+</html>
